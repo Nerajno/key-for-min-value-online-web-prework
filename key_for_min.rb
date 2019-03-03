02c3704143b813b55464e6f2da243baafe4353a7
active-object-6703 <nerajno@gmail.com>
2019-03-03 13:46:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  for element, key in name_hash
-  puts "THe value of this is #{element} and #{key}!"
+  vals = 1000000
+  low_key = ''
+  for key,val in name_hash
+    puts key=>val
+    if val < vals
+      vals = val
+      low_key = key
     end
+  end
+  return low_key
 end
 
 hash = {:blake => 500, :ashley => 2, :adam => 1}
